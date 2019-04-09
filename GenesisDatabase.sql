94e61aea530fca7ce82e32ddfe5825555eb82a72
sdominique43 <46795431+sdominique43@users.noreply.github.com>
2019-04-09 15:46:30 -0400

    Add files via upload

---
+++ GenesisDatabase.sql
@@ -0,0 +1,55 @@
+﻿DROP table if  exists user_inform, like_dislike_songs, playlist_songs;
+
+create table user_inform(
+Username text,
+User_ID VARCHAR(5), 
+primary key(User_ID)
+);
+
+create table like_dislike_songs(
+User_ID VARCHAR(5),  /* if user hits this number send */ 
+Song_ID VARCHAR(4), /* could this be a integer */
+Song_name text, /* to print out the song name */
+Artist_name text,
+Album_name text, 
+Genre_name text, 
+Artist_ID integer, 
+Album_ID integer, 
+Genre_ID integer,
+reaction boolean,  /* user have two options true = like or false = dislike */ 
+primary key(Song_ID), 
+foreign key(User_ID) references user_inform(User_ID)
+); 
+
+create table playlist_songs(
+User_ID VARCHAR(5),
+Song_ID VARCHAR(4),  /* could this be a integer */
+Artist_ID integer,
+Album_ID integer,
+Genre_ID integer,  
+primary key(Song_ID), 
+foreign key(User_ID) references user_inform(User_ID)
+);
+
+INSERT INTO user_inform(User_ID, Username) VALUES
+(67930, 'Serge Dominique'),
+(59372, 'Kimberley DuBois');
+
+INSERT INTO like_dislike_songs(User_ID, Song_ID, Song_Name, Artist_Name, Album_Name,Genre_Name,Artist_ID, Album_ID,Genre_ID,reaction) VALUES 
+(67930, 1390, 'Needed', 'Brent Faiyaz', 'Sonder Son' , 'R&B' , 1, 18, 4, true), 
+(67930, 1378, 'L.A', 'Brent Faiyaz','Sonder Son', 'R&B', 1, 18, 4, false),
+(67930, 3495, 'Home Invasion', 'YNW Melly', 'I Am You','Hip-Hop', 4, 7, 76, false),
+(59372, 4903, 'Chosen', 'Blood Orange', 'Cupid Deluxe', 'Alternative', 2, 20, 9, false),
+(59372, 9506, 'WTS', 'PnB Rock', 'Catch These Vibes','Hip-Hop', 6, 23, 2, true);
+
+INSERT INTO playlist_songs(User_ID, Song_ID, Artist_ID, Album_ID, Genre_ID) VALUES
+(67930, 1390, 1, 18, 4),
+(67930, 1378, 1, 18, 4),
+(59372, 4903, 2, 20, 9),
+(59372, 9506, 6, 23, 2);
+
+select * from user_inform;
+select * from like_dislike_songs;
+select * from like_dislike_songs Where reaction = false; 
+select * from like_dislike_songs Where reaction = true; 
+select * from playlist_songs; 
