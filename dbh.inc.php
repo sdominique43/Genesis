c7f56ac9b938126937b6ba5c9b5f90ad6a11f1c9
sdominique43 <46795431+sdominique43@users.noreply.github.com>
2019-05-08 10:45:19 -0400

    Add files via upload

---
+++ dbh.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+  $servername = "genesisdatabasethesecondcoming.crlaiqpdndku.us-east-2.rds.amazonaws.com";
+  $username = "GenesisCreators";
+  $password = "wewillpassthisclass";
+  $dbname = "GenesisDatabase";
+
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+?>
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+   include_once 'includes/dbh.inc.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Connecting to Database</title>
+</head>
+<body>
+
+<?php
+ $sql = "SELECT * FROM user_inform";
+ $result = mysqli_query($conn , $sql);
+ $resultCheck = mysqli_num_rows($result);
+
+ if ($resultCheck > 0){
+    while ($row = mysqli_fetch_assoc($result)){
+         echo $row['User_ID'];
+    }
+ }
+?>
+
+</body>
+</html>
